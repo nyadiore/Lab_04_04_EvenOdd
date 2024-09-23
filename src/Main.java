e7f268ce1ce362db264558ca09bfe432987cee10
nyadiore <robinn6@mail.uc.edu>
2024-09-23 17:12:40 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+
+
+
+        if (numToExamine % 2 == 0) {
+
+            System.out.println(numToExamine + " is Even.");
+
+        } else {
+
+            System.out.println(numToExamine + " is Odd.");
+
+        }
+
+    }
+
+}
